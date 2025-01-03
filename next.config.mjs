a0a9be6f549f47593bcc0c925bef9eb826acd413
shubham21222 <shubhamraikwar@hotmail.com>
2025-01-03 19:30:18 +0530

    change in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['www.auctionzip.com','auction.nyelizabeth.com','beta.nyelizabeth.com','images.liveauctioneers.com','p1.liveauctioneers.com'], // Add other domains if needed
-    },
-    async rewrites() {
-      return [
-        {
-          source: '/graphql',
-          destination: 'https://auction.nyelizabeth.com/graphql',
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
+
+  async redirects() {
+    return [
+      {
+        source: '/auctions', // The route to be redirected
+        destination: '/find-auctions-near-me', // The target route
+        permanent: true, // Indicates a 301 (permanent) redirect
+      },
+    ];
+  },
+  images: {
+    domains: ['www.auctionzip.com', 'auction.nyelizabeth.com', 'beta.nyelizabeth.com', 'images.liveauctioneers.com', 'p1.liveauctioneers.com'], // Add other domains if needed
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/graphql',
+        destination: 'https://auction.nyelizabeth.com/graphql',
+      },
+    ];
+  },
+};
+
+export default nextConfig;
