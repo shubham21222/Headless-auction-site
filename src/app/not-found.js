19aaba7ec8d293eeaaf920d8206ae0ec3d583713
shubham21222 <shubhamraikwar@hotmail.com>
2025-01-19 11:31:10 +0530

    added 404 page for handle error

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,20 +1,27 @@
 'use client'
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import Header from "@/components/Header";
+import Header2 from "@/components/Header2";
+import Footer from "@/components/Footer";
 
 export default function NotFound() {
   const router = useRouter();
 
-  useEffect(() => {
-    router.push("/");
-  }, []);
+  // useEffect(() => {
+  //   router.push("/");
+  // }, []);
 
   return (
+    <>
+    <Header2 />
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-800">Page Not Found</h1>
-      <p className="mt-4 text-lg text-gray-600">
+      <h1 className="text-4xl font-bold text-gray-800">Error 404 Page Not Found</h1>
+      {/* <p className="mt-4 text-lg text-gray-600">
         Redirecting to Home...
-      </p>
+      </p> */}
     </div>
+    <Footer/>
+    </>
   );
 }
